69aba9459702c4987083ef9d4283c663b9e82dfd
Gopikishan Thawre <gopikishanthawre@gmail.com>
2022-01-26 17:34:51 +0530

    Navbar header, middleware authentication for api, routes updated

--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,11 @@ router.get('/google/callback', passport.authenticate('google',{
     }
 );
 
+// @desc    Logout user
+// @route   auth/logout
+router.get('/logout', (req,res)=>{
+    req.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const router = express.Router();
 
+// importing middlewares to check whether user is authenticated or not and redirecting to corresponding appropriate url's
+const {ensureAuth, ensureGuest} = require('../middleware/auth');
+
 // Login/Landing page
 // GET 
-router.get('/', (req,res)=>{
+router.get('/', ensureGuest ,(req,res)=>{
     res.render('Login', {
         layout : 'login'
     });
 });
 
-router.get('/dashboard', (req,res)=>{
+router.get('/dashboard', ensureAuth ,(req,res)=>{
     res.render('Dashboard',{
         layout: 'main'
     });
